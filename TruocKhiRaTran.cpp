0c24aca3957192be883c748b38d8f85b78d1f106
unknown <phamngochoat30@gmail.com>
2020-09-26 22:27:24 +0700

    Hoat da chinh sua

--- TruocKhiRaTran.cpp
+++ TruocKhiRaTran.cpp
@@ -4,15 +4,17 @@
 using namespace std;
 
 int tongChan(int n);
+int tongLe(int n);
 
 int main()
 {
 	int n;
 	cout << "Nhap vao n = ";
 	cin >> n;
-	cout << "\nTong SO LE tu 1 den " << n << " la: " << tongChan(n) << endl;
+	cout << "\nTong so chan tu 1 den " << n << " la: " << tongChan(n) << endl;
 	cout << "Kha dep trai nhat" << endl;
-	
+	cout << "\nTong so le tu 1 den " << n << " la: " << tongLe(n) << endl;
+	cout << "Kha dep trai nhat" << endl;	
 	getch();
 	return 0;
 }
@@ -25,5 +27,13 @@ int tongChan(int n)
 		if (i % 2 == 0)
 			sum += i;
 	}
+	return sum;
+}
+
+int tongLe(int n)
+{
+	int sum = 0;
+	for (int i = 1; i <= n; i+=2)
+		sum += i;
 	return sum;
 }
