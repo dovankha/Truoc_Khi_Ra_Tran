d377ae36e163bb7ddf40abe8d4f71e600a6911cd
Kha Đỗ Văn <dovankha.tlvn@gmail.com>
2020-09-26 22:08:09 +0700

    Khap update code tong cac so chan

--- TruocKhiRaTran.cpp
+++ TruocKhiRaTran.cpp
@@ -3,10 +3,27 @@
 
 using namespace std;
 
+int tongChan(int n);
+
 int main()
 {
+	int n;
+	cout << "Nhap vao n = ";
+	cin >> n;
+	cout << "\nTong SO LE tu 1 den " << n << " la: " << tongChan(n) << endl;
 	cout << "Kha dep trai nhat" << endl;
 	
 	getch();
 	return 0;
+}
+
+int tongChan(int n)
+{
+	int sum = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 2 == 0)
+			sum += i;
+	}
+	return sum;
 }
