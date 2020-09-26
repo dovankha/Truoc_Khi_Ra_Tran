c7f8384be90ca08692969b0e389dd82b8c858842
Đỗ Xuân Trung <doxuantrung20000@gmail.com>
2020-09-26 22:28:43 +0700

    trung dep chai stylelist

--- TruocKhiRaTran.cpp
+++ TruocKhiRaTran.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int tongChan(int n);
+int CongTu1DenN(int n);
 
 int main()
 {
@@ -12,12 +13,10 @@ int main()
 	cin >> n;
 	cout << "\nTong SO LE tu 1 den " << n << " la: " << tongChan(n) << endl;
 	cout << "Kha dep trai nhat" << endl;
-	
+	cout << "\nKet qua cong tu 1 den n: " << CongTu1DenN(n);
 	getch();
 	return 0;
 }
-
-int tongChan(int n)
 {
 	int sum = 0;
 	for (int i = 1; i <= n; i++)
@@ -26,4 +25,13 @@ int tongChan(int n)
 			sum += i;
 	}
 	return sum;
+}
+int CongTu1DenN(int n)
+{
+	int s = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		s += i;
+	}
+	return s;
 }
